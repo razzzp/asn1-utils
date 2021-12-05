ad3438c0186ce073b6e4a598a7af7d68f22d4f1b
razzzp <m.razif.pramuda@gmail.com>
2021-12-05 21:07:17 +0700

    lexer ok for current test, but code is spaghetti

--- src/asn1/lexers.py
+++ src/asn1/lexers.py
@@ -22,14 +22,129 @@ class LexerState (Enum):
     IN_LEXEME = 1 
     IN_WHITESPACE = 2
 
-SingleCharLexeme = {
-    '=' : 'equal',
+Keywords = {
+    'ABSENT',
+    'ABSTRACT-SYNTAX',
+    'ALL',
+    'APPLICATION',
+    'AUTOMATIC',
+    'BEGIN',
+    'BIT',
+    'BMPString',
+    'BOOLEAN',
+    'BY',
+    'CHARACTER',
+    'CHOICE',
+    'CLASS',
+    'COMPONENT',
+    'COMPONENTS',
+    'CONSTRAINED',
+    'CONTAINING',
+    'DATE',
+    'DATE-TIME',
+    'DEFAULT',
+    'DEFINITIONS',
+    'DURATION',
+    'EMBEDDED',
+    'ENCODED',
+    'ENCODING-CONTROL',
+    'END',
+    'ENUMERATED',
+    'EXCEPT',
+    'EXPLICIT',
+    'EXPORTS',
+    'EXTENSIBILITY',
+    'EXTERNAL',
+    'FALSE',
+    'FROM',
+    'GeneralizedTime',
+    'GeneralString',
+    'GraphicString',
+    'IA5String',
+    'IDENTIFIER',
+    'IMPLICIT',
+    'IMPLIED',
+    'IMPORTS',
+    'INCLUDES',
+    'INSTANCE',
+    'INSTRUCTIONS',
+    'INTEGER',
+    'INTERSECTION',
+    'ISO646String',
+    'MAX',
+    'MIN',
+    'MINUS-INFINITY',
+    'NOT-A-NUMBER',
+    'NULL',
+    'NumericString',
+    'OBJECT',
+    'ObjectDescriptor',
+    'OCTET',
+    'OF',
+    'OID-IRI',
+    'OPTIONAL',
+    'PATTERN',
+    'PDV',
+    'PLUS-INFINITY',
+    'PRESENT',
+    'PrintableString',
+    'PRIVATE',
+    'REAL',
+    'RELATIVE-OID',
+    'RELATIVE-OID-IRI',
+    'SEQUENCE',
+    'SET',
+    'SETTINGS',
+    'SIZE',
+    'STRING',
+    'SYNTAX',
+    'T61String',
+    'TAGS',
+    'TeletexString',
+    'TIME',
+    'TIME-OF-DAY',
+    'TRUE',
+    'TYPE-IDENTIFIER',
+    'UNION',
+    'UNIQUE',
+    'UNIVERSAL',
+    'UniversalString',
+    'UTCTime',
+    'UTF8String',
+    'VideotexString',
+    'VisibleString',
+    'WITH',   
+}
+
+MultiCharSymbol = {
+    '.' : 'dot',
+    ':' : 'colon',
+    '[' : 'start_square_brace',
+    ']' : 'end_square_brace',
+    '*' : 'asterik',
+    '/' : 'forward_slash',
+    '-' : 'hypen'
+    # </ and /> not supported
+}
+
+MultiCharSymbolLexemes ={
+    '..' : 'double_dot',
+    '[[' : 'double_start_square_brace',
+    ']]' : 'double_end_square_brace',
+    '/*' : 'start comment',
+    '*/' : 'end_comment',
+    '--' : 'single line comment'
+}
+
+SingleCharSymbol = {
+    '=' : 'equals',
     '\'' : 'quote',
     '\"' : 'double_quotes',
     ',' : 'comma',
-    '-' : 'hypen',
-    '/' : 'slash',
-    ':' : 'colon',
+    # hypens can be in identifiers
+    #'-' : 'hypen',
+    #'/' : 'slash',
+    #':' : 'colon',
     ';' : 'semi_colon',
     '@' : 'at',
     '|' : 'pipe',
@@ -37,11 +152,11 @@ SingleCharLexeme = {
     '^' : 'caret',
      '(' : 'start_brace',
     '{' : 'start_curly_brace',
-    '[' : 'start_square_brace',
+    #'[' : 'start_square_brace',
     '<' : 'less_than',
      ')' : 'end_brace',
     '}' : 'end_curly_brace',
-    ']' : 'end_square_brace',
+    #']' : 'end_square_brace',
     '>' : 'greater_than',
 }
 
@@ -79,35 +194,81 @@ class ASN1Lexer (Lexer):
                 if cur_lexeme != '':
                     result.append(cur_lexeme)
                 break
-            # state machine 
-            if prev_char not in WhiteSpace and prev_char not in SingleCharLexeme:
+            # state machine
+            if prev_char.isalnum() and prev_char not in WhiteSpace:
+            # prev char is alpha-numeric
                 if cur_char in WhiteSpace:
+                    # end of current lexeme, append and reset
                     result.append(cur_lexeme)
                     cur_lexeme=''
-                elif cur_char in SingleCharLexeme:
+
+                elif cur_char in MultiCharSymbol:
+                    # if multi char symbol, just append lexeme
+                    # don't append cur char
+                    if cur_char != '-':
+                        result.append(cur_lexeme)
+                        cur_lexeme = cur_char
+                    else:
+                        # single hypen can be part of identifier
+                        # continue
+                        cur_lexeme += cur_char
+                elif cur_char in SingleCharSymbol:
                     # if prev_char not whitespace, and cur_char is single char lexeme
-                    # add cur_lexeme and the cur char as lexeme
+                    # end of cur_lexeme                   
                     result.append(cur_lexeme)
+                    cur_lexeme= ''
+                    # also add the cur char as lexeme
                     result.append(cur_char)
+                elif cur_char not in WhiteSpace:
+                    # prev_char not whitespace & cur_char is whitespace
+                    # continue adding chars
+                    cur_lexeme += cur_char
+            elif prev_char in MultiCharSymbol:
+                cur_symbol = prev_char + cur_char
+                if cur_symbol in MultiCharSymbolLexemes:
+                    # case comment after alpha num
+                    if cur_symbol == '--' and len(cur_lexeme) > 2:
+                        # identifier is everthing except last char ('-')
+                        result.append(cur_lexeme[0:-1])
+                    result.append(cur_symbol)
                     cur_lexeme=''
-                elif cur_char not in WhiteSpace:
-                    # prev_char not whitespace & cur_char is whitespace
+                elif cur_char == '=' and cur_lexeme == '::':
                     cur_lexeme += cur_char
+                    result.append(cur_lexeme)
+                    cur_lexeme=''
+                elif cur_char == ':' and prev_char == ':':
+                    # continue
+                    cur_lexeme += cur_char
+                elif cur_char == '.' and cur_lexeme == '..':
+                    cur_lexeme += cur_char
+                    result.append(cur_lexeme)
+                    cur_lexeme=''
+                else:
+                    if cur_char in SingleCharSymbol:
+                        result.append(cur_lexeme)
+                        result.append(cur_char)
+                    elif cur_char not in WhiteSpace:
+                        cur_lexeme += cur_char
+                # TODO comments
             else:
-                if cur_char in SingleCharLexeme:
-                    result.append(cur_char)
+                if cur_char in SingleCharSymbol:
+                    if cur_lexeme != '':
+                        result.append(cur_lexeme)
+                        cur_lexeme = ''
+                    result.append(cur_char)     
                 elif cur_char not in WhiteSpace:
                     # prev_char not whitespace & cur_char is whitespace
                     cur_lexeme += cur_char
-                
             prev_char = cur_char
         return result
 
     def _get_tokens(self, lexemes : List[str]) -> List[Token]:
         result = []
-        for (lexeme, i) in enumerate(lexemes):
+        for (i, lexeme) in enumerate(lexemes):
+            # TODO
+            result.append(lexeme)
             pass
-        return lexemes
+        return result
     
     def _next_state(self, input : str, state : LexerState) -> LexerState:
         if WhiteSpace[input]:
--- src/tests/lexer_test.py
+++ src/tests/lexer_test.py
@@ -15,7 +15,7 @@ def run():
     BEGIN
 
     -- Basic integer types, for size constraints
-    maxUInt8 INTEGER ::= 255
+    maxUInt8 INTEGER ::=255
     UInt8 ::= INTEGER (0..maxUInt8)
     maxUInt15 INTEGER ::= 32767
     UInt15 ::= INTEGER (0..maxUInt15)
@@ -25,7 +25,145 @@ def run():
     UInt31 ::= INTEGER (0..maxUInt31)
 
     ApplicationIdentifier ::= OCTET STRING (SIZE(5..16))
+
+    PEHeader ::= SEQUENCE {
+    mandated NULL OPTIONAL,
+    -- if set, indicate that the support of this PE is mandatory
+    identification UInt15 -- Identification number of this PE
+    }
+
+    ProfileElement ::= CHOICE {
+	header ProfileHeader,
+
+/* PEs */
+	genericFileManagement PE-GenericFileManagement,
+	pinCodes PE-PINCodes,
+	pukCodes PE-PUKCodes,
+	akaParameter PE-AKAParameter,
+	cdmaParameter PE-CDMAParameter,
+	securityDomain PE-SecurityDomain,
+	rfm PE-RFM,
+	application PE-Application,
+	nonStandard PE-NonStandard,
+	end PE-End,
+	rfu1 PE-Dummy, -- this avoids renumbering of tag values
+	rfu2 PE-Dummy, -- in case other non-file-system PEs are
+	rfu3 PE-Dummy, -- added here in future versions
+	rfu4 PE-Dummy,
+	rfu5 PE-Dummy,
+
+/* PEs related to file system creation using templates defined in this specification */	
+	mf PE-MF,
+	cd PE-CD,
+	telecom PE-TELECOM,
+	usim PE-USIM,
+	opt-usim PE-OPT-USIM,
+	isim PE-ISIM,
+	opt-isim PE-OPT-ISIM,
+	phonebook PE-PHONEBOOK,
+	gsm-access PE-GSM-ACCESS,
+	csim PE-CSIM,
+	opt-csim PE-OPT-CSIM,
+	eap PE-EAP,
+	df-5gs PE-DF-5GS,
+	df-saip PE-DF-SAIP,
+...
+}
     END
+    """)
+
+    keywords = StringIO("""
+     ABSENT
+ABSTRACT-SYNTAX
+ALL
+APPLICATION
+AUTOMATIC
+BEGIN
+BIT
+BMPString
+BOOLEAN
+BY
+CHARACTER
+CHOICE
+CLASS
+COMPONENT
+COMPONENTS
+CONSTRAINED
+CONTAINING
+DATE
+DATE-TIME
+DEFAULT
+DEFINITIONS
+DURATION
+EMBEDDED
+ENCODED
+ENCODING-CONTROL
+END
+ENUMERATED
+EXCEPT
+EXPLICIT
+EXPORTS
+EXTENSIBILITY
+EXTERNAL
+FALSE
+FROM
+GeneralizedTime
+GeneralString
+GraphicString
+IA5String
+IDENTIFIER
+IMPLICIT
+IMPLIED
+IMPORTS
+INCLUDES
+INSTANCE
+INSTRUCTIONS
+INTEGER
+INTERSECTION
+ISO646String
+MAX
+MIN
+MINUS-INFINITY
+NOT-A-NUMBER
+NULL
+NumericString
+OBJECT
+ObjectDescriptor
+OCTET
+OF
+OID-IRI
+OPTIONAL
+PATTERN
+PDV
+PLUS-INFINITY
+PRESENT
+PrintableString
+PRIVATE
+REAL
+RELATIVE-OID
+RELATIVE-OID-IRI
+SEQUENCE
+SET
+SETTINGS
+SIZE
+STRING
+SYNTAX
+T61String
+TAGS
+TeletexString
+TIME
+TIME-OF-DAY
+TRUE
+TYPE-IDENTIFIER
+UNION
+UNIQUE
+UNIVERSAL
+UniversalString
+UTCTime
+UTF8String
+VideotexString
+VisibleString
+WITH
     """)
     print('out:\n')
     lexer = lexers.ASN1Lexer()
