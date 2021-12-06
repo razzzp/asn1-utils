ac9d86f8fe9a3e5c64cc2c03b2db3a5458a4bc70
razzzp <m.razif.pramuda@gmail.com>
2021-12-06 22:23:41 +0700

    a bit less spaghetti lexer

--- src/asn1/lexers.py
+++ src/asn1/lexers.py
@@ -2,6 +2,7 @@ from io import StringIO
 from sys import stdout
 from enum import Enum
 from typing import List
+from asn1.dicts import *
 
 class TokenType (Enum):
     IDENTIFIER = 0,
@@ -17,248 +18,187 @@ class Token:
         self.name = name
         pass
 
-class LexerState (Enum):
-    START = 0
-    IN_LEXEME = 1 
-    IN_WHITESPACE = 2
-
-Keywords = {
-    'ABSENT',
-    'ABSTRACT-SYNTAX',
-    'ALL',
-    'APPLICATION',
-    'AUTOMATIC',
-    'BEGIN',
-    'BIT',
-    'BMPString',
-    'BOOLEAN',
-    'BY',
-    'CHARACTER',
-    'CHOICE',
-    'CLASS',
-    'COMPONENT',
-    'COMPONENTS',
-    'CONSTRAINED',
-    'CONTAINING',
-    'DATE',
-    'DATE-TIME',
-    'DEFAULT',
-    'DEFINITIONS',
-    'DURATION',
-    'EMBEDDED',
-    'ENCODED',
-    'ENCODING-CONTROL',
-    'END',
-    'ENUMERATED',
-    'EXCEPT',
-    'EXPLICIT',
-    'EXPORTS',
-    'EXTENSIBILITY',
-    'EXTERNAL',
-    'FALSE',
-    'FROM',
-    'GeneralizedTime',
-    'GeneralString',
-    'GraphicString',
-    'IA5String',
-    'IDENTIFIER',
-    'IMPLICIT',
-    'IMPLIED',
-    'IMPORTS',
-    'INCLUDES',
-    'INSTANCE',
-    'INSTRUCTIONS',
-    'INTEGER',
-    'INTERSECTION',
-    'ISO646String',
-    'MAX',
-    'MIN',
-    'MINUS-INFINITY',
-    'NOT-A-NUMBER',
-    'NULL',
-    'NumericString',
-    'OBJECT',
-    'ObjectDescriptor',
-    'OCTET',
-    'OF',
-    'OID-IRI',
-    'OPTIONAL',
-    'PATTERN',
-    'PDV',
-    'PLUS-INFINITY',
-    'PRESENT',
-    'PrintableString',
-    'PRIVATE',
-    'REAL',
-    'RELATIVE-OID',
-    'RELATIVE-OID-IRI',
-    'SEQUENCE',
-    'SET',
-    'SETTINGS',
-    'SIZE',
-    'STRING',
-    'SYNTAX',
-    'T61String',
-    'TAGS',
-    'TeletexString',
-    'TIME',
-    'TIME-OF-DAY',
-    'TRUE',
-    'TYPE-IDENTIFIER',
-    'UNION',
-    'UNIQUE',
-    'UNIVERSAL',
-    'UniversalString',
-    'UTCTime',
-    'UTF8String',
-    'VideotexString',
-    'VisibleString',
-    'WITH',   
-}
-
-MultiCharSymbol = {
-    '.' : 'dot',
-    ':' : 'colon',
-    '[' : 'start_square_brace',
-    ']' : 'end_square_brace',
-    '*' : 'asterik',
-    '/' : 'forward_slash',
-    '-' : 'hypen'
-    # </ and /> not supported
-}
-
-MultiCharSymbolLexemes ={
-    '..' : 'double_dot',
-    '[[' : 'double_start_square_brace',
-    ']]' : 'double_end_square_brace',
-    '/*' : 'start comment',
-    '*/' : 'end_comment',
-    '--' : 'single line comment'
-}
-
-SingleCharSymbol = {
-    '=' : 'equals',
-    '\'' : 'quote',
-    '\"' : 'double_quotes',
-    ',' : 'comma',
-    # hypens can be in identifiers
-    #'-' : 'hypen',
-    #'/' : 'slash',
-    #':' : 'colon',
-    ';' : 'semi_colon',
-    '@' : 'at',
-    '|' : 'pipe',
-    '!' : 'exclamation',
-    '^' : 'caret',
-     '(' : 'start_brace',
-    '{' : 'start_curly_brace',
-    #'[' : 'start_square_brace',
-    '<' : 'less_than',
-     ')' : 'end_brace',
-    '}' : 'end_curly_brace',
-    #']' : 'end_square_brace',
-    '>' : 'greater_than',
-}
-
-WhiteSpace = {
-    ' ' : 'space',
-    '\n' : 'newline',
-    '\t' : 'tab'
-}
-
 class Lexer:
+    # maybe this will be used
     pass
 
 class ASN1Lexer (Lexer):
     def __init__(self) -> None:
         super().__init__()
-        self._state = LexerState.START
         self._tokens : List[str] = []
         self._cur_lexeme = ''
 
     def tokenize(self, istream : StringIO):
-        lexemes = self._get_lexemes(istream)
+        lexemes = self._get_lexemes_v2(istream)
         #
         tokens = self._get_tokens(lexemes)
         return tokens
 
-    def _get_lexemes(self, istream : StringIO) -> List[str]:
+    # @DeprecationWarning
+    # def _get_lexemes(self, istream : StringIO) -> List[str]:
+    #     """Deprecated because of spaghetti"""
+    #     result = []
+    #     prev_char = ' '
+    #     cur_char = ''
+    #     cur_lexeme = ''
+
+    #     while True:
+    #         cur_char = istream.read(1)
+    #         if cur_char == '':
+    #             if cur_lexeme != '':
+    #                 result.append(cur_lexeme)
+    #             break
+    #         # state machine
+    #         if prev_char.isalnum() and prev_char not in WhiteSpace:
+    #         # prev char is alpha-numeric
+    #             if cur_char in WhiteSpace:
+    #                 # end of current lexeme, append and reset
+    #                 result.append(cur_lexeme)
+    #                 cur_lexeme=''
+
+    #             elif cur_char in MultiCharSymbol:
+    #                 # if multi char symbol, just append lexeme
+    #                 # don't append cur char
+    #                 if cur_char != '-':
+    #                     result.append(cur_lexeme)
+    #                     cur_lexeme = cur_char
+    #                 else:
+    #                     # single hypen can be part of identifier
+    #                     # continue
+    #                     cur_lexeme += cur_char
+    #             elif cur_char in SingleCharSymbol:
+    #                 # if prev_char not whitespace, and cur_char is single char lexeme
+    #                 # end of cur_lexeme                   
+    #                 result.append(cur_lexeme)
+    #                 cur_lexeme= ''
+    #                 # also add the cur char as lexeme
+    #                 result.append(cur_char)
+    #             elif cur_char not in WhiteSpace:
+    #                 # prev_char not whitespace & cur_char is whitespace
+    #                 # continue adding chars
+    #                 cur_lexeme += cur_char
+    #         elif prev_char in MultiCharSymbol:
+    #             cur_symbol = prev_char + cur_char
+    #             if cur_symbol in MultiCharSymbolLexemes:
+    #                 # case comment after alpha num
+    #                 if cur_symbol == '--' and len(cur_lexeme) > 2:
+    #                     # identifier is everthing except last char ('-')
+    #                     result.append(cur_lexeme[0:-1])
+    #                 result.append(cur_symbol)
+    #                 cur_lexeme=''
+    #             elif cur_char == '=' and cur_lexeme == '::':
+    #                 cur_lexeme += cur_char
+    #                 result.append(cur_lexeme)
+    #                 cur_lexeme=''
+    #             elif cur_char == ':' and prev_char == ':':
+    #                 # continue
+    #                 cur_lexeme += cur_char
+    #             elif cur_char == '.' and cur_lexeme == '..':
+    #                 cur_lexeme += cur_char
+    #                 result.append(cur_lexeme)
+    #                 cur_lexeme=''
+    #             else:
+    #                 if cur_char in SingleCharSymbol:
+    #                     result.append(cur_lexeme)
+    #                     result.append(cur_char)
+    #                 elif cur_char not in WhiteSpace:
+    #                     cur_lexeme += cur_char
+    #             # TODO comments
+    #         else:
+    #             if cur_char in SingleCharSymbol:
+    #                 if cur_lexeme != '':
+    #                     result.append(cur_lexeme)
+    #                     cur_lexeme = ''
+    #                 result.append(cur_char)     
+    #             elif cur_char not in WhiteSpace:
+    #                 # prev_char not whitespace & cur_char is whitespace
+    #                 cur_lexeme += cur_char
+    #         prev_char = cur_char
+    #     return result
+
+    def _get_lexemes_v2(self, istream : StringIO) -> List[str]:
         result = []
         prev_char = ' '
         cur_char = ''
         cur_lexeme = ''
+        in_singleline_comment = False
+        in_multiline_comment = False
 
         while True:
             cur_char = istream.read(1)
             if cur_char == '':
+                # if empty means end of file
                 if cur_lexeme != '':
                     result.append(cur_lexeme)
                 break
             # state machine
-            if prev_char.isalnum() and prev_char not in WhiteSpace:
-            # prev char is alpha-numeric
-                if cur_char in WhiteSpace:
-                    # end of current lexeme, append and reset
-                    result.append(cur_lexeme)
-                    cur_lexeme=''
-
-                elif cur_char in MultiCharSymbol:
-                    # if multi char symbol, just append lexeme
-                    # don't append cur char
-                    if cur_char != '-':
-                        result.append(cur_lexeme)
-                        cur_lexeme = cur_char
-                    else:
-                        # single hypen can be part of identifier
-                        # continue
-                        cur_lexeme += cur_char
-                elif cur_char in SingleCharSymbol:
-                    # if prev_char not whitespace, and cur_char is single char lexeme
-                    # end of cur_lexeme                   
-                    result.append(cur_lexeme)
-                    cur_lexeme= ''
-                    # also add the cur char as lexeme
-                    result.append(cur_char)
-                elif cur_char not in WhiteSpace:
-                    # prev_char not whitespace & cur_char is whitespace
-                    # continue adding chars
-                    cur_lexeme += cur_char
-            elif prev_char in MultiCharSymbol:
-                cur_symbol = prev_char + cur_char
-                if cur_symbol in MultiCharSymbolLexemes:
-                    # case comment after alpha num
-                    if cur_symbol == '--' and len(cur_lexeme) > 2:
-                        # identifier is everthing except last char ('-')
-                        result.append(cur_lexeme[0:-1])
-                    result.append(cur_symbol)
-                    cur_lexeme=''
-                elif cur_char == '=' and cur_lexeme == '::':
-                    cur_lexeme += cur_char
-                    result.append(cur_lexeme)
-                    cur_lexeme=''
-                elif cur_char == ':' and prev_char == ':':
-                    # continue
-                    cur_lexeme += cur_char
-                elif cur_char == '.' and cur_lexeme == '..':
-                    cur_lexeme += cur_char
-                    result.append(cur_lexeme)
-                    cur_lexeme=''
+            if in_multiline_comment or in_singleline_comment:
+                # in comment block
+                if in_multiline_comment and prev_char + cur_char == '*/':
+                    in_multiline_comment = False
+                    result.append(cur_lexeme + cur_char)
+                    cur_lexeme =''
+                elif in_singleline_comment and cur_char == '\n':
+                    in_singleline_comment = False
+                    result.append(cur_lexeme + cur_char)
+                    cur_lexeme =''
                 else:
-                    if cur_char in SingleCharSymbol:
-                        result.append(cur_lexeme)
-                        result.append(cur_char)
-                    elif cur_char not in WhiteSpace:
-                        cur_lexeme += cur_char
-                # TODO comments
-            else:
-                if cur_char in SingleCharSymbol:
+                    # still in comment continue
+                    cur_lexeme += cur_char
+                continue
+            if cur_char in WhiteSpace:
+                # if whitespace, whatever is in cur_lexeme is added if not empty
+                if cur_lexeme != '':
+                    result.append(cur_lexeme)
+                    cur_lexeme = ''
+                pass
+            elif cur_char in SingleCharSymbol:
+                if cur_char == '=' and cur_lexeme == '::':
+                    result.append('::=')
+                    cur_lexeme = ''
+                else:
                     if cur_lexeme != '':
                         result.append(cur_lexeme)
                         cur_lexeme = ''
-                    result.append(cur_char)     
-                elif cur_char not in WhiteSpace:
-                    # prev_char not whitespace & cur_char is whitespace
+                    # also append the Single Char Symbol
+                    result.append(cur_char)
+                pass
+            elif cur_char in MultiCharSymbol:
+                combined = cur_lexeme + cur_char
+                if combined[-2:] == '--':
+                    # for case cur_lexeme = 'blabla-'
+                    prev_lexeme = cur_lexeme[0:-1]
+                    if prev_lexeme != '':
+                        result.append(prev_lexeme)
+                    # start comment
+                    in_singleline_comment = True
+                    cur_lexeme = '--'
+                    pass
+                elif combined == '/*':
+                    # append all except last char
+                    result.append(cur_lexeme[0:-1])
+                    # start comment
+                    in_multiline_comment = True
+                    cur_lexeme = '/*'
+                    pass
+                elif combined == '::':
+                    # add and continue
                     cur_lexeme += cur_char
+                elif combined in MultiCharSymbolLexemes:
+                    result.append(combined)
+                    cur_lexeme = ''
+                else:
+                    # start new multi char lexeme
+                    if cur_lexeme != '' and cur_char != '-':
+                        result.append(cur_lexeme)
+                        cur_lexeme = cur_char
+                    else:
+                        # continue
+                        cur_lexeme += cur_char
+            elif cur_char.isalnum():
+                cur_lexeme += cur_char
+            else:
+                raise ValueError(f'unsupported char found at {istream.tell()}: \'{cur_char}\'')
             prev_char = cur_char
         return result
 
@@ -269,10 +209,4 @@ class ASN1Lexer (Lexer):
             result.append(lexeme)
             pass
         return result
-    
-    def _next_state(self, input : str, state : LexerState) -> LexerState:
-        if WhiteSpace[input]:
-            return LexerState.IN_WHITESPACE
-        else:
-            return LexerState.IN_LEXEME
 
